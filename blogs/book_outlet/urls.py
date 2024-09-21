5f6b7f00350b19808e89f2fb7f2fe5cdc8e115ee
copyNinja1219 <thapacyrus30@gmail.com>
2024-09-21 19:10:13 +0530

    Feat : Use correct method for adding URLs

--- blogs/book_outlet/urls.py
+++ blogs/book_outlet/urls.py
@@ -2,7 +2,13 @@ from django.urls import path
 
 from . import views
 
+# urlpatterns = [
+#     path("", views.index),
+#     path("<int:id>", views.book_detail)
+# ]
+
+
 urlpatterns = [
     path("", views.index),
-    path("<int:id>", views.book_detail)
+    path("<slug:slug>", views.book_detail, name="book-detail")
 ]
