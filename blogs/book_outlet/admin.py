8d32765a932bbeb7e317adcf7e2d140ae8491fce
copyNinja1219 <thapacyrus30@gmail.com>
2024-09-22 00:43:59 +0530

    Feat : Add admin dashboard customizables

--- blogs/book_outlet/admin.py
+++ blogs/book_outlet/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book
+
+# admin.site.register(Book)
+# Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    # readonly_fields = ("slug",) # here we are saying that it should not be editable hence it will throw error !
+    # because of this we dont need save() method to be overridden
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "rating",)
+    list_display = ("title", "author",)
+
+
+admin.site.register(Book, BookAdmin)
