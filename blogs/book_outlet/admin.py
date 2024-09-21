fa3b9a39cd943912abdd1de897f91d3a9af8bf7c
copyNinja1219 <thapacyrus30@gmail.com>
2024-09-22 01:26:55 +0530

    Feat : Add Address models for author as 1-1 field

--- blogs/book_outlet/admin.py
+++ blogs/book_outlet/admin.py
@@ -1,18 +1,35 @@
-from django.contrib import admin
+# from django.contrib import admin
 
-# Register your models here.
-from .models import Book
+# # Register your models here.
+# from .models import Book
 
 # admin.site.register(Book)
 # Register your models here.
 
 
+# class BookAdmin(admin.ModelAdmin):
+#     # readonly_fields = ("slug",) # here we are saying that it should not be editable hence it will throw error !
+#     # because of this we dont need save() method to be overridden
+#     prepopulated_fields = {"slug": ("title",)}
+#     list_filter = ("author", "rating",)
+#     list_display = ("title", "author",)
+
+
+# admin.site.register(Book, BookAdmin)
+
+from django.contrib import admin
+
+from .models import Book, Author, Address
+
+# Register your models here.
+
+
 class BookAdmin(admin.ModelAdmin):
-    # readonly_fields = ("slug",) # here we are saying that it should not be editable hence it will throw error !
-    # because of this we dont need save() method to be overridden
     prepopulated_fields = {"slug": ("title",)}
     list_filter = ("author", "rating",)
     list_display = ("title", "author",)
 
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
--- blogs/book_outlet/models.py
+++ blogs/book_outlet/models.py
@@ -1,3 +1,38 @@
+# from django.core import validators
+# from django.db import models
+# from django.core.validators import MinValueValidator, MaxValueValidator
+# from django.urls import reverse
+# from django.utils.text import slugify
+
+# # Create your models here.
+
+
+# class Author(models.Model):
+#     first_name = models.CharField(max_length=100)
+#     last_name = models.CharField(max_length=100)
+
+
+# class Book(models.Model):
+#     title = models.CharField(max_length=50)
+#     rating = models.IntegerField(
+#         validators=[MinValueValidator(1), MaxValueValidator(5)])
+#     author = models.CharField(null=True, max_length=100)
+#     is_bestselling = models.BooleanField(default=False)
+#     # Harry Potter 1 => harry-potter-1
+#     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+#     # editable=False, null=False, db_index=True)
+
+#     def get_absolute_url(self):
+#         return reverse("book-detail", args=[self.slug])
+
+#     # def save(self, *args, **kwargs):
+#     #     self.slug = slugify(self.title)
+#     #     super().save(*args, **kwargs)
+#     # prepopulated_fields = {"slug": ("title",)} this will do the work now
+
+#     def __str__(self):
+#         return f"{self.title} ({self.rating})"
+
 from django.core import validators
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
@@ -7,28 +42,43 @@ from django.utils.text import slugify
 # Create your models here.
 
 
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.street}, {self.postal_code}, {self.city}"
+
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(
+        Address, on_delete=models.CASCADE, null=True)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
 
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
     # Harry Potter 1 => harry-potter-1
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
-    # editable=False, null=False, db_index=True)
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
-
-    # def save(self, *args, **kwargs):
-    #     self.slug = slugify(self.title)
-    #     super().save(*args, **kwargs)
-    # prepopulated_fields = {"slug": ("title",)} this will do the work now
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
