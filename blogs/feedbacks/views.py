d44a822212b15520ecf6865dcf4eca40e8530380
copyNinja1219 <thapacyrus30@gmail.com>
2024-09-27 00:08:01 +0530

    Fix : Use modal class

--- blogs/feedbacks/views.py
+++ blogs/feedbacks/views.py
@@ -39,11 +39,15 @@ def review(request):
         form = ReviewForm(request.POST)
 
         if form.is_valid():
-            review = Review(
-                user_name=form.cleaned_data['user_name'],
-                review_text=form.cleaned_data['review_text'],
-                rating=form.cleaned_data['rating'])
-            review.save()
+            form.save()
+        # form = ReviewForm(request.POST)
+
+        # if form.is_valid():
+        #     review = Review(
+        #         user_name=form.cleaned_data['user_name'],
+        #         review_text=form.cleaned_data['review_text'],
+        #         rating=form.cleaned_data['rating'])
+        #     review.save()
             return HttpResponseRedirect("/reviews/thank-you")
 
     else:
