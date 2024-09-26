4e1628d9a65c646587c99983f667ac30716dc3a2
copyNinja1219 <thapacyrus30@gmail.com>
2024-09-26 23:30:38 +0530

    Feat : Add review modal & connect the form to backend

--- blogs/feedbacks/views.py
+++ blogs/feedbacks/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from .forms import ReviewForm
+from .models import Review
 
 # Create your views here.
 
@@ -38,17 +39,34 @@ def review(request):
         form = ReviewForm(request.POST)
 
         if form.is_valid():
-            """
-            valid input & is required & cleans & validated data 
-            """
-            print(form.cleaned_data)
-            return HttpResponseRedirect("/thank-you")
+            review = Review(
+                user_name=form.cleaned_data['user_name'],
+                review_text=form.cleaned_data['review_text'],
+                rating=form.cleaned_data['rating'])
+            review.save()
+            return HttpResponseRedirect("/reviews/thank-you")
 
-    form = ReviewForm()
+    else:
+        form = ReviewForm()
 
-    return render(request, "reviews/review.html", {
+    return render(request, "feedbacks/review.html", {
         "form": form
     })
+    # if request.method == 'POST':
+    #     form = ReviewForm(request.POST)
+
+    #     if form.is_valid():
+    #         """
+    #         valid input & is required & cleans & validated data
+    #         """
+    #         print(form.cleaned_data)
+    #         return HttpResponseRedirect("/reviews/thank-you")
+
+    # form = ReviewForm()
+
+    # return render(request, "feedbacks/review.html", {
+    #     "form": form
+    # })
 
 
 def thank_you(request):
