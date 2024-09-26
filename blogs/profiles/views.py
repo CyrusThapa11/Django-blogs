47b0fd2a81a0bfe968cbeb3146352d7102bf3edc
copyNinja1219 <thapacyrus30@gmail.com>
2024-09-27 02:08:24 +0530

    Feat : Add modal for file uploads

--- blogs/profiles/views.py
+++ blogs/profiles/views.py
@@ -3,30 +3,32 @@ from django.views import View
 from django.http import HttpResponseRedirect
 
 from .forms import ProfileForm
+from .models import UserProfile
 
 # Create your views here.
 
 
-def store_file(file):
-    with open("temp/image.jpg", "wb+") as dest:
-        for chunk in file.chunks():
-            dest.write(chunk)
+# def store_file(file):
+#     with open("temp/image.jpg", "wb+") as dest:
+#         for chunk in file.chunks():
+#             dest.write(chunk)
 
 
 class CreateProfileView(View):
     def get(self, request):
-        # form = ProfileForm()
-        # return render(request, "profiles/create_profile.html", {
-        #     "form": form
-        # })
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {
+            "form": form
+        })
+        # return render(request, "profiles/create_profile.html")
 
     def post(self, request):
-        # submitted_form = ProfileForm(request.POST, request.FILES)
+        submitted_form = ProfileForm(request.POST, request.FILES)
 
-        # if submitted_form.is_valid():
-        store_file(request.FILES["image"])
-        return HttpResponseRedirect("/profiles")
+        if submitted_form.is_valid():
+            profile = UserProfile(image=request.FILES["user_image"])
+            profile.save()
+            return HttpResponseRedirect("/profiles")
 
         return render(request, "profiles/create_profile.html", {
             "form": submitted_form
