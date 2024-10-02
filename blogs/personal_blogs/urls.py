cad0d73d29dd67b082d48fd6230d099fa9c1305d
copyNinja1219 <thapacyrus30@gmail.com>
2024-10-02 13:03:10 +0530

    Feat : Add save later functionality

--- blogs/personal_blogs/urls.py
+++ blogs/personal_blogs/urls.py
@@ -18,7 +18,8 @@ urlpatterns = [
 
     path("", views.StartingPageView.as_view(), name="starting-page"),
     path("posts", views.AllPostsView.as_view(), name="posts-page"),
+    path("posts/read-later", views.ReadLaterView.as_view(), name="read-later"),
     path("posts/<slug:slug>", views.SinglePostView.as_view(),
-         name="post-detail")  # /posts/my-first-post
+         name="post-detail"),  # /posts/my-first-post
 
 ]
